bc129790a7327deab8c98b8e1a32352a0d75017e
Mohammed Owais <osowais78@gmail.com>
2026-07-10 21:26:55 +0530

    First Code of for loops

---
+++ Lec04_Loop01.java
@@ -0,0 +1,7 @@
+public class Lec04_Loop01 {
+    public static void main(String[] args){
+        for(int counter = 0;counter < 40;counter = counter + 1){
+            System.out.println("I am learning java");
+        }
+    }
+}
