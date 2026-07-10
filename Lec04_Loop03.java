613fb36f4d901c057c2cdd8b0006b6cc8515b351
Mohammed Owais <osowais78@gmail.com>
2026-07-10 23:13:26 +0530

    while loop implemetation

---
+++ Lec04_Loop03.java
@@ -0,0 +1,13 @@
+public class Lec04_Loop03 {
+    public static void main(String[] args){
+        //using while loop 
+    int i = 0;
+
+    while(i<11){
+        System.out.println(i);
+        i++;
+    }
+    }
+    
+}
+ 
