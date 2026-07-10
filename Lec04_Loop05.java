03d67bcea59513236a585d376c2039d19354d3c6
Mohammed Owais <osowais78@gmail.com>
2026-07-10 23:37:25 +0530

    minor changes

--- Lec04_Loop05.java
+++ Lec04_Loop05.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 public class Lec04_Loop05 {
     public static void main(String[] args){
         //Print the sum of n natural numbers like n=5
-        int n = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+
         int sum = 0;
-        for(int i = 1; i<=n; i++){
+        for(int i=1;i<=n;i++){
             sum = sum+i;
         }
-        System.out.println(sum);
+        System.out.println("The sum of n natural numbers is :"+sum);
     }
 }
- 
