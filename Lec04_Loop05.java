0ab2cde07133d28371ce546316bb87a7557e8750
Mohammed Owais <osowais78@gmail.com>
2026-07-10 23:27:18 +0530

    finding the sum of n numbers using for loop

---
+++ Lec04_Loop05.java
@@ -0,0 +1,12 @@
+public class Lec04_Loop05 {
+    public static void main(String[] args){
+        //Print the sum of n natural numbers like n=5
+        int n = 5;
+        int sum = 0;
+        for(int i = 1; i<=n; i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
+ 
