d1a2f99c9b30ebc1b82f8e8932ab2b252687393c
Mohammed Owais <osowais78@gmail.com>
2026-07-10 21:42:22 +0530

    Printing 1-10 using loop

---
+++ Lec04_Loop02.java
@@ -0,0 +1,7 @@
+public class Lec04_Loop02 {
+    public static void main(String[] args){
+        for(int count = 0; count < 11; count++){
+            System.out.println(count);
+        }
+    }
+}
