de6f9b3c3a5c7c0ca6b17eaf0baea47a097e5f54
Mohammed Owais <osowais78@gmail.com>
2026-07-11 10:39:03 +0530

    Printing the table of the number entered by the user.

---
+++ Lec04_Loop06.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Lec04_Loop06 {
+    public static void main(String[] args){
+        //Pirnt table of a number input by the user.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the the number of which the table needs to be printed");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for(int i=1;i<11;i++){
+            sum = n*i;
+            System.out.println(sum);
+        }
+    }
+}
