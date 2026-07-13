dd2bfe85f5f1c5c2419fab0f112fa60838e931f4
Mohammed Owais <osowais78@gmail.com>
2026-07-13 10:44:14 +0530

    Half Pyramid Pattern problem

---
+++ Lec05_patterns_HalfPyramid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Lec05_patterns_HalfPyramid {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+
+        
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
