c3cf3db4bae200ec4174418bafea8db565feebfc
Mohammed Owais <osowais78@gmail.com>
2026-07-13 09:55:57 +0530

    I did it again wowwww

---
+++ Lec05_patterns3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Lec05_patterns3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        int b = sc.nextInt();
+
+        for(int i=1;i<a;i++){
+            for(int j=1;j<b;j++){
+                System.out.print("$");
+            }
+            System.out.println("$");
+        }
+    }
+}
