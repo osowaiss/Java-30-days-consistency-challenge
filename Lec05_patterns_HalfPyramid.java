24cfd36155096b4ab12ab2460bd622da19d96579
Mohammed Owais <osowais78@gmail.com>
2026-07-13 12:08:11 +0530

    Inverted Half Pyramid Pattern

--- Lec05_patterns_HalfPyramid.java
+++ Lec05_patterns_HalfPyramid.java
@@ -2,11 +2,13 @@ import java.util.*;
 
 public class Lec05_patterns_HalfPyramid {
     public static void main(String[] args){
+        //Write a program to achive a pattern of a Half Pyramid
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of a");
         int a = sc.nextInt();
 
-        
+
         for(int i=1;i<=a;i++){
             for(int j=1;j<i;j++){
                 System.out.print("*");
