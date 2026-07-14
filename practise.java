66f964bacfe6157d191ce012e477b1418cca58f9
Mohammed Owais <osowais78@gmail.com>
2026-07-14 15:50:05 +0530

    practising patterns

---
+++ practise.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class practise {
+    public static void main(String[] args){
+        int a =5;
+        int b =6;
+
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=b; j++){
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
+}
