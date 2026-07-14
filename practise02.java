89604855c1ce105be6676577b93a29dd8dae6f5c
Mohammed Owais <osowais78@gmail.com>
2026-07-14 16:31:01 +0530

    practising the hollow pattern problem

---
+++ practise02.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class practise02 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the vlaue of a ");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b");
+        int b = sc.nextInt();
+
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(i==1 || i==a || j==1 || j==b){
+                    System.out.print("$");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
