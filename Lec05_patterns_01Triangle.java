295d533146a8ce4b864a6fc9077e3cfec2376cca
Mohammed Owais <osowais78@gmail.com>
2026-07-17 10:55:19 +0530

    Butterfly pattern

---
+++ Lec05_patterns_01Triangle.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Lec05_patterns_01Triangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n to see the Butterfly pattern formed");
+        int n = sc.nextInt();
+        
+        //Upper half
+        for(int i=1;i<=n;i++){
+            
+            //1st part of stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces
+            int spaces = 2*(n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+
+            //2nd part of stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Lower Half
+        for(int i=n;i>=1;i--){
+            
+            //1st part of stars
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            //spaces
+            int spaces = 2*(n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+
+            //2nd part of stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
